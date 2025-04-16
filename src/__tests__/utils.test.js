a78c01308cff155f54cb509efc28f2f3c9224cb9
Frankincense <frankincensewelsey@gmail.com>
2025-04-16 12:56:20 +0300

    tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,22 @@
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
+  test("returns the age of a person based on the year of birth", () => {
+    // Arrange
+    const currentYear = new Date().getFullYear();
     const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Assert
+    expect(ageOfPerson).toEqual(answer);
+  })
+  test("returns the current year for a person born in year 0", () => {
+    const currentYear = new Date().getFullYear();
+    const birthYear = 0;
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+    expect(ageOfPerson).toBe(currentYear);
   });
 });
